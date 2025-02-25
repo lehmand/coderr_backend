c7fcbccc4bfe4784db3851b521ad165ceee247e7
Daniel Lehmann <daniel.lehmann188@gmail.com>
2025-02-25 20:26:02 +0100

    added main app

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path, include
+from django.urls import path
 from .views import RegistrationView
 
 urlpatterns = [
     path('registration/', RegistrationView.as_view(), name='register'),
+    path('login/', ),
 ]
--- coderr_backend/urls.py
+++ coderr_backend/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('auth_app.api.urls')),
+    path('api/', include('auth_app.api.urls')),
+    path('api/', include('main.api.urls')),
 ]
