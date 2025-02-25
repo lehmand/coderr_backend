7407518149b0ce08c2135ac4be2e33ffa230a895
Daniel Lehmann <daniel.lehmann188@gmail.com>
2025-02-25 21:13:04 +0100

    deleted main_app and added offers app and included offers.urls in project urls

--- coderr_backend/urls.py
+++ coderr_backend/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('auth_app.api.urls')),
-    path('api/', include('main.api.urls')),
+    path('api/', include('offers.api.urls')),
 ]
