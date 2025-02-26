a03b9e0c192e65803b5de7c9928e2616d72a55f1
Daniel Lehmann <139554573+lehmand@users.noreply.github.com>
2025-02-26 08:31:25 +0000

    changed User and UserProfile mechanic

--- auth_app/api/serializers.py
+++ auth_app/api/serializers.py
@@ -6,11 +6,11 @@ from rest_framework.authtoken.models import Token
 class RegistrationSerializer(serializers.ModelSerializer):
     
     repeated_password = serializers.CharField(write_only=True)
-    type = serializers.ChoiceField(choices=User.PROFILE_TYPES)
+    type = serializers.ChoiceField(choices=UserProfile.PROFILE_TYPES)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'type']
+        fields = ['username', 'email', 'password', 'repeated_password', 'type']
         extra_kwargs = {'password': {'write_only': True}}
 
     def validate(self, data):
@@ -22,6 +22,7 @@ class RegistrationSerializer(serializers.ModelSerializer):
     
     def create(self, validated_data):
         validated_data.pop('repeated_password')
+        validated_data.pop('type')
         user = User.objects.create_user(**validated_data)
         Token.objects.create(user=user)
         return user
--- coderr/settings.py
+++ coderr/settings.py
@@ -133,8 +133,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'auth_app.User'
-
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
