65cf5f89975469c59565d3c1b06320eb70eb36a5
Daniel Lehmann <139554573+lehmand@users.noreply.github.com>
2025-02-26 08:37:38 +0000

    reworked registration works

--- auth_app/api/serializers.py
+++ auth_app/api/serializers.py
@@ -5,23 +5,19 @@ from rest_framework.authtoken.models import Token
 
 class RegistrationSerializer(serializers.ModelSerializer):
     
-    repeated_password = serializers.CharField(write_only=True)
     type = serializers.ChoiceField(choices=UserProfile.PROFILE_TYPES)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'repeated_password', 'type']
+        fields = ['username', 'email', 'password', 'type']
         extra_kwargs = {'password': {'write_only': True}}
 
     def validate(self, data):
-        if data['password'] != data['repeated_password']:
-            raise serializers.ValidationError({'password': ['Die Passwörter stimmen nicht überein.']})   
         if User.objects.filter(email=data['email']).exists():
             raise serializers.ValidationError({'email': ['Diese E-mail wird bereits verwendet.']})
         return data
     
     def create(self, validated_data):
-        validated_data.pop('repeated_password')
         validated_data.pop('type')
         user = User.objects.create_user(**validated_data)
         Token.objects.create(user=user)
