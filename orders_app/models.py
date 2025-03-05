bd4b33338d7a760ffe20f9942af68a83a6919c83
Daniel Lehmann <daniel.lehmann188@gmail.com>
2025-03-05 20:05:49 +0100

    created Order model

--- orders_app/models.py
+++ orders_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Order(models.Model):
+
+    customer_user = models.IntegerField()
+    business_user = models.IntegerField()
+    title = models.CharField(max_length=200)
+    revisions = models.IntegerField()
+    delivery_time_in_days = models.IntegerField()
+    price = models.IntegerField()
+    features = models.CharField(max_length=200)
+    offer_type = models.CharField(max_length=200)
+    status = models.CharField(max_length=200, default='in_progress')
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
