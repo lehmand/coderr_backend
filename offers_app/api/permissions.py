72ebbab15ed4081be1a6a0f60c6c2776a93fe48f
Daniel Lehmann <daniel.lehmann188@gmail.com>
2025-03-07 22:22:05 +0100

    offer permissions wip

--- offers_app/api/permissions.py
+++ offers_app/api/permissions.py
@@ -12,3 +12,14 @@ class IsBusinessUser(permissions.BasePermission):
             return True
         
         return user_profile.type == 'business'
+    
+class IsCreator(permissions.BasePermission):
+
+    message = 'Nur Ersteller können ein Angebot bearbeiten oder löschen.'
+
+    def has_object_permission(self, request, view, obj):
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        return obj.user_id == request.user.id
--- offers_app/api/views.py
+++ offers_app/api/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics
 from offers_app.models import Offer, OfferDetail
 from .serializers import OfferCreateSerializer, OfferListSerializer, OfferDetailSerializer, SingleOfferSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny, IsAuthenticatedOrReadOnly
-from .permissions import IsBusinessUser
+from .permissions import IsBusinessUser, IsCreator
 
 class OfferListCreateView(generics.ListCreateAPIView):
     queryset = Offer.objects.all()
@@ -24,7 +24,15 @@ class SingleOfferView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Offer.objects.all()
     serializer_class = OfferCreateSerializer
 
+    def get_permissions(self):
+        if self.request.method in ['PATCH', 'DELETE']:
+            permission_classes = [IsAuthenticated, IsCreator]
+        else:
+            permission_classes = [AllowAny]
+        return [permission() for permission in permission_classes]
+
 
 class OfferDetailView(generics.RetrieveAPIView):
     queryset = OfferDetail.objects.all()
     serializer_class = OfferDetailSerializer
+    permission_classes = [AllowAny]
