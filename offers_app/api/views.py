a2c0960b7b74da80e3e26e76a230a4b7cc223fd7
Daniel Lehmann <daniel.lehmann188@gmail.com>
2025-03-07 21:50:38 +0100

    offer permissions wip

--- offers_app/api/views.py
+++ offers_app/api/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics
 from offers_app.models import Offer, OfferDetail
 from .serializers import OfferCreateSerializer, OfferListSerializer, OfferDetailSerializer, SingleOfferSerializer
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAuthenticatedOrReadOnly
+from .permissions import IsBusinessUser
 
 class OfferListCreateView(generics.ListCreateAPIView):
     queryset = Offer.objects.all()
@@ -10,6 +11,14 @@ class OfferListCreateView(generics.ListCreateAPIView):
         if self.request.method == 'GET':
             return OfferListSerializer
         return OfferCreateSerializer
+    
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            permission_classes = [IsAuthenticated, IsBusinessUser]
+        else:
+            permission_classes = [IsAuthenticatedOrReadOnly]
+        return [permission() for permission in permission_classes]
+    
 
 class SingleOfferView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Offer.objects.all()
